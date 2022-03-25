1749ded25e0aedb31f6c475b3c5ed01b09e3feb0
ak <2414075840@qq.com>
2022-03-25 13:49:49 +0800

    添加文件

---
+++ gitWorks/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @ProjectName: git_workspace
+ * @Package: com.atguigu.git
+ * @ClassName: HelloGit
+ * @Author: ak
+ * @Description: ${description}
+ * @Date: 2022/3/25 2:31
+ * @Version: 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
